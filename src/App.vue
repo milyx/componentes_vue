4d493a7f813dfc5442c59efd0758cbe533b2cfb6
milagros acosta <milyarias91@gmail.com>
2022-03-15 12:22:48 -0300

    end changes

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,62 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <FigurAnimada
+      v-for="(personaje, i) in personajes"
+      :key="i"
+      :lolito="personaje"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import FigurAnimada from "@/components/FigurAnimada.vue";
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    FigurAnimada,
+  },
+  data() {
+    return {
+      personajes: [
+        {
+          name: "Morty Smith",
+          image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+        },
+        {
+          name: "Morty Smith",
+          image: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+        },
+        {
+          name: "Summer Smith",
+          image: "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
+        },
+        {
+          name: "Beth Smith",
+          image: "https://rickandmortyapi.com/api/character/avatar/4.jpeg",
+        },
+        {
+          name: "Beth Smith",
+          image: "https://rickandmortyapi.com/api/character/avatar/5.jpeg",
+        },
+        {
+          name: "Jerry Smith",
+          image: "https://rickandmortyapi.com/api/character/avatar/6.jpeg",
+        },
+      ],
+    };
+  },
+  img() {
+    return {
+      fondo: [
+        {
+          src: "https://rickandmortypod.com/wp-content/uploads/2018/11/cropped-RM_page-header_background1-3.png",
+        },
+      ],
+    };
+  },
+
+  props: {
+    text: String,
   },
 };
 </script>
@@ -22,7 +67,9 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #097737;
+
+  padding: 30px 50px;
+  background-image: url(https://rickandmortypod.com/wp-content/uploads/2018/11/cropped-RM_page-header_background1-3.png);
 }
 </style>
---
+++ src/components/FigurAnimada.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="content d-flex flex-start">
+      <img
+        :src="lolito.image"
+        :alt="lolito.name"
+        width="100"
+        height="auto"
+        class="py-2 img"
+      />
+      <h3 class="py-5 px-2">
+        {{ lolito.name }}
+      </h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FigurAnimada",
+  props: {
+    lolito: {
+      type: Object,
+      default: () => {},
+    },
+  },
+};
+</script>
+
+<style>
+.img {
+  border-radius: 50%;
+}
+</style>
